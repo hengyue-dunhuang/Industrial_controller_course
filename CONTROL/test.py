afc1e5c76f29dcc3941f7fc860bf43b6273c9a6c
hengyue-dunhuang <1342906871@qq.com>
2023-03-14 14:27:02 +0800

    实现了机械臂的逆运动学

--- CONTROL/test.py
+++ CONTROL/test.py
@@ -1,6 +1,10 @@
-CONTROL_BUFFER = ['aa','5a','5a','5a','5a','5a','5a','01','ff']
-control_bytes=bytearray(9)
-def control_bytes_init():
-    for i in range(9):
-        control_bytes[i:i+1]=bytes.fromhex(CONTROL_BUFFER[i])
-control_bytes_init()
+from math import acos
+import math
+def get_theta(a:float,b:float,c:float) -> int:
+    '''通过余弦定理，给定三角形的两对边和临边，求出该角角度（°）'''
+    theta =  acos((pow(a,2)+pow(b,2)-pow(c,2))/(2*a*b)) 
+    theta = theta/math.pi * 180
+    return int(theta)
+x = get_theta(1,1.414,1)
+print(x)
+
